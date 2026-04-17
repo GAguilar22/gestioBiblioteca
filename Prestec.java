6c00ddff9670d1a4c8178fedaff7ecb9d4a34f23
Roberto Miguélez <roberto.miguelez@insbaixcamp.cat>
2026-04-17 13:25:53 +0200

    primer commit Robert

--- Prestec.java
+++ Prestec.java
@@ -1,16 +1,61 @@
 import java.time.LocalDate;
+
 public class Prestec {
-private Usuari usuari;
-private Llibre llibre;
-private LocalDate dataPrestec;
-private LocalDate dataRetorn;
-public Prestec(Usuari usuari, Llibre llibre, LocalDate dataPrestec) {
-this.usuari = usuari;
-this.llibre = llibre;
-this.dataPrestec = dataPrestec;
-this.dataRetorn = dataPrestec.plusWeeks(2);
-}
-public Usuari getUsuari() { return usuari; }
-public Llibre getLlibre() { return llibre; }
-public LocalDate getDataRetorn() { return dataRetorn; }
+    private Usuari usuari;
+    private Llibre llibre;
+    private LocalDate dataPrestec;
+    private LocalDate dataRetorn;
+    private boolean retornat;
+
+    private static final int DIES_PRESTEC = 14;
+
+    public Prestec(Usuari usuari, Llibre llibre) {
+        this.usuari = usuari;
+        this.llibre = llibre;
+        this.dataPrestec = LocalDate.now();
+        this.dataRetorn = dataPrestec.plusDays(DIES_PRESTEC);
+        this.retornat = false;
+    }
+
+    public Usuari getUsuari() {
+        return usuari;
+    }
+
+    public Llibre getLlibre() {
+        return llibre;
+    }
+
+    public LocalDate getDataPrestec() {
+        return dataPrestec;
+    }
+
+    public LocalDate getDataRetorn() {
+        return dataRetorn;
+    }
+
+    public boolean isRetornat() {
+        return retornat;
+    }
+
+    public void retornar() {
+        if (!retornat) {
+            llibre.retornar();
+            usuari.retornarLlibre(llibre);
+            retornat = true;
+            System.out.println("Llibre retornat correctament.");
+        } else {
+            System.out.println("Aquest préstec ja està tancat.");
+        }
+    }
+
+    public boolean estaEndarrerit() {
+        return LocalDate.now().isAfter(dataRetorn) && !retornat;
+    }
+
+    @Override
+    public String toString() {
+        return llibre.getTitol() + " prestat a " + usuari.getNom() +
+                " fins a " + dataRetorn +
+                (retornat ? " (Retornat)" : (estaEndarrerit() ? " (Endarrerit)" : ""));
+    }
 }
--- Usuari.java
+++ Usuari.java
@@ -1,14 +1,57 @@
 import java.util.ArrayList;
 import java.util.List;
+
 public class Usuari {
-private String nom;
-private List<Llibre> llibresPrestats;
-public Usuari(String nom) {
-this.nom = nom;
-this.llibresPrestats = new ArrayList<>();
-}
-public String getNom() { return nom; }
-public List<Llibre> getLlibresPrestats() { return llibresPrestats; }
-public void afegirLlibre(Llibre llibre) { llibresPrestats.add(llibre); }
-public void retornarLlibre(Llibre llibre) { llibresPrestats.remove(llibre); }
+    private String nom;
+    private List<Llibre> llibresPrestats;
+    private static final int MAX_LLIBRES = 3; // màxim llibres per usuari
+
+    public Usuari(String nom) {
+        this.nom = nom;
+        this.llibresPrestats = new ArrayList<>();
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public List<Llibre> getLlibresPrestats() {
+        return llibresPrestats;
+    }
+
+    public boolean potPrestar() {
+        return llibresPrestats.size() < MAX_LLIBRES;
+    }
+
+    public void afegirLlibre(Llibre llibre) {
+        if (potPrestar()) {
+            llibresPrestats.add(llibre);
+        } else {
+            System.out.println("L'usuari ja té el màxim de llibres prestats.");
+        }
+    }
+
+    public void retornarLlibre(Llibre llibre) {
+        if (llibresPrestats.contains(llibre)) {
+            llibresPrestats.remove(llibre);
+        } else {
+            System.out.println("Aquest llibre no el té aquest usuari.");
+        }
+    }
+
+    public void mostrarLlibres() {
+        if (llibresPrestats.isEmpty()) {
+            System.out.println(nom + " no té llibres prestats.");
+        } else {
+            System.out.println("Llibres de " + nom + ":");
+            for (Llibre l : llibresPrestats) {
+                System.out.println("- " + l);
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Usuari: " + nom + " | Llibres prestats: " + llibresPrestats.size();
+    }
 }
